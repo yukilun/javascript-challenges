7babd9e8dddb8c968964110298fd466b78f27b97
yukilun <102889935+yukilun@users.noreply.github.com>
2023-10-10 02:01:32 -0700

    Fix HTML validation issue

--- components/Stamp.vue
+++ components/Stamp.vue
@@ -3,8 +3,8 @@
         :style="`top: ${stamp.y}px; left: ${stamp.x}px; transform:translate(-50%, -50%) rotate(${stamp.rotate}deg); color:${stamp.color}`">
         <div v-if="stamp.styleId <= 1" class="p-1 lg:p-2" :class="stamp.styleId == 0 ? 'border-y-8' : 'border-8 rounded-2xl'"
             :style="`border-color: ${stamp.color}`">
-            <div class="px-3 py-2 lg:px-6 lg:py-4" :class="stamp.styleId == 0 ? 'border-y-2' : 'border-2 rounded-lg'" :style="`border-color: ${stamp.color}`">
-                <h1>{{ stamp.text }}</h1>
+            <div class="px-3 py-2 lg:px-6 lg:py-4 font-heading" :class="stamp.styleId == 0 ? 'border-y-2' : 'border-2 rounded-lg'" :style="`border-color: ${stamp.color}`">
+                {{ stamp.text }}
             </div>
         </div>
         <div v-else class="w-16 h-16  lg:w-24 lg:h-24">
