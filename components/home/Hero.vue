ef34b1ff760bf5c946756e599172857f9958459a
yukilun <102889935+yukilun@users.noreply.github.com>
2023-10-10 02:32:35 -0700

    Typo

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -3,7 +3,7 @@
         flex justify-center items-center text-center relative z-0">
         <section id="hero" class="w-full max-w-[250px] md:max-w-[800px]">
             <h1 class="block text-5xl md:text-6xl lg:text-7xl mb-6">
-                JavaScript Challenge
+                JavaScript Challenges
             </h1>
             <h2 class="text-lg md:text-xl lg:text-2xl text-neutral-500 dark:text-neutral-400 mb-20">
                 Enhance my JavaScript skills through continuous challenges.
